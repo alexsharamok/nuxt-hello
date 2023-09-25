8329727d0b9891df0d46ea284041a2569ecd5d0f
alex sharamok <alexanders@uniform.dev>
2023-09-25 16:33:09 +0300

    changed header

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
   security: {
     headers: {
       contentSecurityPolicy: {
-        'frame-ancestors': ["'self', 'https://uniform.app'"],
+        'frame-ancestors': ["'self', https://uniform.app"],
       }
     }
   },
