3cee476fd7fba2c03b1e33eb830c18ec2ac3336f
alex sharamok <alexanders@uniform.dev>
2023-09-25 17:34:57 +0300

    credentialless header

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
   security: {
     headers: {
       crossOriginResourcePolicy: 'cross-origin',
-      crossOriginEmbedderPolicy: '',
+      crossOriginEmbedderPolicy: 'credentialless',
       contentSecurityPolicy: {
         'frame-ancestors': ["'self' https://uniform.app"],
       },      
